d5a53b814ef92bb1ededf7640bf6171b80a451c2
David Stroud <>
2024-11-13 17:32:06 -0600

    Decompose Quicksorter

--- src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
@@ -42,18 +42,16 @@ public class Quicksorter<T> implements Sorter<T> {
   // +---------+
 
   /**
-   * Sorts a slice of an array in place.
-   * @param values The full array to sort.
-   * @param start The first index to sort, inclusive.
-   * @param end The last index to sort, exclusive.
+   * Partitions a sub-array into elements less than, equal to, and greater than an arbitrary pivot.
+   * @param values The array to sort.
+   * @param start The first index of the sub-array, inclusive.
+   * @param end The last index of the sub-array, exclusive.
+   * @return
+   *   An array with two elements:
+   *   the start of the "equal to" section,
+   *   and the start of the "greater than" section.
    */
-  private void sortSlice(T[] values, int start, int end) {
-    if ((end - start) < 2) {
-      // This slice is already sorted, since it either has
-      // one or zero elements.
-      return;
-    } // if
-
+  private int[] partition(T[] values, int start, int end) {
     T pivot = values[start / 2 + end / 2];
     int startEqual = start;
     int startGt = end;
@@ -73,8 +71,26 @@ public class Quicksorter<T> implements Sorter<T> {
       } // if-else
     } // for
 
-    this.sortSlice(values, start, startEqual);
-    this.sortSlice(values, startGt, end);
+    return new int[] {startEqual, startGt};
+  } // partition(T[], int, int)
+
+  /**
+   * Sorts a slice of an array in place.
+   * @param values The full array to sort.
+   * @param start The first index to sort, inclusive.
+   * @param end The last index to sort, exclusive.
+   */
+  private void sortSlice(T[] values, int start, int end) {
+    if ((end - start) < 2) {
+      // This slice is already sorted, since it either has
+      // one or zero elements.
+      return;
+    } // if
+
+    int[] indices = this.partition(values, start, end);
+
+    this.sortSlice(values, start, indices[0]);
+    this.sortSlice(values, indices[1], end);
   } // sortSlice(T[], int, int)
 
   /**
